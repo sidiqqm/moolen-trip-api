84922671f19d0ee801b72fea42a7d93608e4e0b5
Muhammad Sidiq <moehammad.siddiq@gmail.com>
2025-06-25 23:55:02 +0700

    add login and logout api

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,26 +1,74 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
-  
-  console.log(newUser);
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to create user!",
+    });
+  }
 };
 
-export const login = (req, res) => {
-  res.send("berhasil diaktifkan");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check user available
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid Credential" });
+
+    // Check password incorrect
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword)
+      return res.status(401).json({ message: "Password incorrect" });
+
+    // Generate cookie token and send it to user
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      //secure: true,
+      maxAge: age,
+    });
+    res.status(200).json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "Login failed" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("berhasil diaktifkan");
+  res.clearCookie("token").status(200).json({ message: "Logout successful" });
 };
